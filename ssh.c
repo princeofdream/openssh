83a543b20bc8139c04f1b20b5cb1ee50759a1d0b
princeofdream <princeofdream@outlook.com>
2024-07-19 16:04:25 +0800

    Add passwrod argument for ssh and scp

--- ssh.c
+++ ssh.c
@@ -692,6 +692,7 @@ main(int ac, char **av)
 	 * set.
 	 */
 	initialize_options(&options);
+	options.user_passwd = NULL;
 
 	/*
 	 * Prepare main ssh transport/connection structures
@@ -707,7 +708,7 @@ main(int ac, char **av)
 	argv0 = av[0];
 
  again:
-	while ((opt = getopt(ac, av, "1246ab:c:e:fgi:kl:m:no:p:qstvx"
+	while ((opt = getopt(ac, av, "1246ab:c:e:fgi:kl:m:no:p:qstvxz:"
 	    "AB:CD:E:F:GI:J:KL:MNO:PQ:R:S:TVw:W:XYy")) != -1) { /* HUZdhjruz */
 		switch (opt) {
 		case '1':
@@ -1052,6 +1053,9 @@ main(int ac, char **av)
 			break;
 		case 'F':
 			config = optarg;
+			break;
+		case 'z':
+			options.user_passwd = optarg;
 			break;
 		default:
 			usage();
